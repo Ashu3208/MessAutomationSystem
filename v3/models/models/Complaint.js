01e0053d8bce9eacf02dc832bb11abd32041723c
chethan58 <106975695+chethan58@users.noreply.github.com>
2023-03-25 17:49:39 +0530

    Delete v3 directory

--- v3/models/models/Complaint.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require("mongoose")
-
-const complaintSchema = new mongoose.Schema({
-    rollNo:Number,
-    issue:String,
-    reply:String
-})
-
-module.exports = mongoose.model('Complaint',complaintSchema)
