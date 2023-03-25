f204386f92f31584733bc6891a4a5810a29a8941
chethan58 <106975695+chethan58@users.noreply.github.com>
2023-03-25 17:53:04 +0530

    Add files via upload

---
+++ v3/models/Complaint.js
@@ -0,0 +1,9 @@
+const mongoose =require("mongoose")
+
+const complaintSchema = new mongoose.Schema({
+    rollNo:Number,
+    issue:String,
+    reply:String
+})
+
+module.exports = mongoose.model('Complaint',complaintSchema)
---
+++ v3/models/Extra.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const extraSchema = new mongoose.Schema({
+    
+    name:String,
+    price:Number
+})
+
+module.exports = mongoose.model('Extra',extraSchema)
