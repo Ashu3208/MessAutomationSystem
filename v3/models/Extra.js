2ab33f8c24cd3b6d031dfe9f4958c6736eb02c55
chethan58 <106975695+chethan58@users.noreply.github.com>
2023-04-05 01:23:06 +0530

    Delete v3 directory

--- v3/models/Extra.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const extraSchema = new mongoose.Schema({
-    
-    name:String,
-    price:Number
-})
-
-module.exports = mongoose.model('Extra',extraSchema)
