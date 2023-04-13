c82b3fb42f62c596964c3e2efce36b0096e74e7b
chethan58 <charugundlachethan@gmail.com>
2023-04-13 21:36:26 +0530

    minor change

--- project/routes/auth.js
+++ project/routes/auth.js
@@ -1,11 +1,14 @@
 // require express router, passport
 const router = require('express').Router()
 const passport = require('passport')
+
 require("dotenv").config();
 
 // User Model
 const User = require('../models/User')
-
+const axios = require('axios');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 //create passport local strategy
 passport.use(User.createStrategy())
 
@@ -24,10 +27,54 @@ passport.deserializeUser(function (id, done) {
 });
 
 
+// Verify the email address
+axios.get('https://apilayer.net/api/check', {
+    params: {
+        access_key: process.env.MAILBOXLAYER_API_KEY,
+        email,
+    },
+})
+    .then((response) => {
+        const { format_valid, smtp_check } = response.data;
+        if (format_valid && smtp_check) {
+            // The email address is valid and deliverable, so send the OTP email
+            const msg = {
+                to: email,
+                from: 'your-email@example.com',
+                subject: 'Your OTP for website registration',
+                text: `Your OTP is ${otp}`,
+            };
+
+            sgMail.send(msg)
+                .then(() => {
+                    res.send('OTP sent to your email');
+                })
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send('Error sending OTP');
+                });
+        } else {
+            // The email address is invalid or undeliverable, so show an error message
+            res.status(400).send('Invalid email address');
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error verifying email address');
+    });
+
+
+app.post('/register', (req, res) => {
+    const email = req.body.email;
+    const otp = Math.floor(Math.random() * 10000);
+    
+    
+  });
+  
 
 // register user in db
 router.post("/auth/register", async (req, res) => {
-     try {
+    try {
         function isWhitespaceString(str) {
             return /^\s*$/.test(str);
         }
@@ -40,29 +87,30 @@ router.post("/auth/register", async (req, res) => {
 
                 if (emailRegex.test(email)) {
                     console.log('Valid email');
-                  const existingUser = await User.findOne({ username: req.body.username });
-                  const existingroll=await User.findOne({rollNumber:req.body.rollNumber})
-                if (existingUser ) {
-                // username already exists, send an alert to the user
-                return res.send(`<script>alert("Username already exists. Please choose a different username."); window.location.href='/register';</script>`);
-                }else if(existingroll){
-                    return res.send(`<script>alert("Roll number already exists. Please choose a different roll number."); window.location.href='/register';</script>`);
-                } 
-                else {
-                const registerUser = await User.register({
-                    name: req.body.name,
-                    rollNumber: req.body.rollNumber,
-                    username: req.body.username,
-                    extrasCost: 0,
-                    rebateDays: 0,
-                    dues: 0
-                }, req.body.password);
-                if (registerUser) {
-                    res.redirect("/login");
-                } else {
-                    res.redirect("/register");
-                }
-                }
+                    const existingUser = await User.findOne({ username: req.body.username });
+                    const existingroll = await User.findOne({ rollNumber: req.body.rollNumber })
+                    if (existingUser) {
+                        // username already exists, send an alert to the user
+                        return res.send(`<script>alert("Sorry, that email address is already in use. Please try logging in or use a different email address to register.
+                        "); window.location.href='/register';</script>`);
+                    } else if (existingroll) {
+                        return res.send(`<script>alert("Roll number already exists. Please choose a different roll number."); window.location.href='/register';</script>`);
+                    }
+                    else {
+                        const registerUser = await User.register({
+                            name: req.body.name,
+                            rollNumber: req.body.rollNumber,
+                            username: req.body.username,
+                            extrasCost: 0,
+                            rebateDays: 0,
+                            dues: 0
+                        }, req.body.password);
+                        if (registerUser) {
+                            res.redirect("/login");
+                        } else {
+                            res.redirect("/register");
+                        }
+                    }
 
                 } else {
                     console.log('Invalid email');
