c2724be2f5f4d9922c5181df1d2bf16bf32bc6b8
chethan58 <charugundlachethan@gmail.com>
2023-04-21 22:23:35 +0530

    fixed manager orders page and now user can withdraw active rebate request prior to its starting date

--- project/routes/managerOrders.js
+++ project/routes/managerOrders.js
@@ -7,9 +7,12 @@ const Order = require("../models/Order");
 router.get("/manager/order", async (req, res) => {
   if (req.isAuthenticated()) {
     if (process.env.SUPERUSER === "true") {
+      let i=0,j=0;
       res.render("manager/order", {
         orders: await Order.find({}),
         session: req.session,
+        i: i,
+        j: j,
       });
     } else {
       res.redirect("/");
@@ -20,22 +23,32 @@ router.get("/manager/order", async (req, res) => {
 });
 
 router.post("/manager/order", async (req, res) => {
-  req.session.flag = 1;
-  req.session.orderId = req.body.orderId;
-  req.session.index = req.body.index;
-  console.log(req.body);
+  try {
+    const parts = req.body.string.split(" ");
+    req.session.flag = 1;
+    const orderId = parts[0]; // First part
+    const index = parts[1]; // Second part
+    req.session.orderId = orderId;
+    req.session.index = index;
+    console.log(req.body);
+  } catch (err) {
+    console.log(err);
+  }
+
   res.redirect("/manager/order");
 });
 
 router.post("/code", async (req, res) => {
-  const order= await Order.findById(req.session.orderId);
-
-  console.log(parseInt(order.code[req.session.index]));
+  const order = await Order.findById(req.session.orderId);
+  console.log(req.session.orderId);
   console.log(parseInt(req.body.code));
-  if(parseInt(req.body.code) === parseInt(order.code[req.session.index]) ){
+  console.log(parseInt(order.code[req.session.index]))
+  if (parseInt(req.body.code) == parseInt(order.code[req.session.index])) {
     req.session.flag = 2;
-    await Order.findByIdAndUpdate(req.session.orderId,{ $set: { [`status.${req.session.index}`]: "cleared" } })
-  }else{
+    await Order.findByIdAndUpdate(req.session.orderId, {
+      $set: { [`status.${req.session.index}`]: "cleared" },
+    });
+  } else {
     req.session.flag = 3;
   }
   console.log(req.body);
